2a11cbe610f197efd62be83b1276b27a172c6d1b
EdwinM96 <edwin.morales9b@gmail.com>
2019-12-22 01:25:01 -0600

    Added SurveyTakerRepo

---
+++ src/main/java/com/survey/repository/SurveyTakerRepository.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.survey.repository;
+
+import org.springframework.stereotype.Repository;
+import com.survey.domain.SurveyTaker;
+import java.io.Serializable;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+/**
+ *
+ * @author HP PC
+ */
+@Repository
+public interface SurveyTakerRepository extends JpaRepository<SurveyTaker, Integer>{
+    
+
+    public List<SurveyTaker> findAll();
+    
+}
