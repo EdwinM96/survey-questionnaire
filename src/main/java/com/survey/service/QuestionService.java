32c586a809cd2341641ef120c057b12cace93c2a
EdwinM96 <edwin.morales9b@gmail.com>
2019-12-22 01:37:38 -0600

    Added QuestionService class

---
+++ src/main/java/com/survey/service/QuestionService.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.survey.service;
+
+import com.survey.repository.QuestionRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.survey.domain.Question;
+/**
+ *
+ * @author HP PC
+ */
+@Service
+public class QuestionService {
+    
+    @Autowired
+    QuestionRepository questionRepo;
+    
+    public List<Question> findAll(){
+        return questionRepo.findAll();
+    }
+    
+}
