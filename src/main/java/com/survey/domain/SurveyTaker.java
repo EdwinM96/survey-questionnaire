0e23ff671c3aca2886fd15a69901638b32928db0
EdwinM96 <edwin.morales9b@gmail.com>
2019-12-22 01:18:22 -0600

    Added SurveyTaker as domain database reference

--- src/main/java/com/survey/domain/SurveyTaker.java
+++ src/main/java/com/survey/domain/SurveyTaker.java
@@ -5,10 +5,56 @@
  */
 package com.survey.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author HP PC
  */
+@Entity
+@Table(schema="public", name="SurveyTaker")
 public class SurveyTaker {
+    
+    @Id
+    @Column(name="SurveyTakerID")
+    private Integer id;
+    
+    @Column(name="Email")
+    private String email;
+    
+    @Column(name="IP")
+    private String varchar;
+
+    public SurveyTaker() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getVarchar() {
+        return varchar;
+    }
+
+    public void setVarchar(String varchar) {
+        this.varchar = varchar;
+    }
+    
+    
     
 }
