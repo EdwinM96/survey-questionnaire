c8a874fde8944b772bbed13fac0a3e919f1481fb
EdwinM96 <edwin.morales9b@gmail.com>
2019-12-22 01:13:18 -0600

    Added Cuestion as domain database reference

---
+++ src/main/java/com/survey/domain/Question.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.survey.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author HP PC
+ */
+@Entity
+@Table(schema="public", name="Question")
+public class Question {
+    
+    @Id
+    @Column(name="QuestionID")
+    private Integer id;
+    
+    @Column(name="Question")
+    private String question;
+
+    public Question() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+    
+    
+    
+}
